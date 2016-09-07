999b7be3203d8ce62e9c5570a3a2d30aec9379c1
jessiefrimodig <jessieasta@att.net>
2016-09-07 10:12:10 -0700

    Added Lesson 2
    Contains Lab 2 and Lesson 2.

---
+++ Pylesson_01/Lab_02.py
@@ -0,0 +1,18 @@
+num1 = 6
+num2 = 8
+product = 6 * 8
+print("6 multiplied by 8 is", num1 * num2)
+
+
+zipcode = " 92130"
+address = "3555 "
+name = "Santoro Way, "
+city = "San Diego,"
+print(address + name + city + zipcode)
+
+
+width = 2
+length = 4
+height = 3
+times = 2
+print("The surface area of your rectangle is", times * width * length + times * length * height + times * height * width)
---
+++ Pylesson_02/PYLesson_02.py
@@ -0,0 +1,5 @@
+l = 8
+w = 5
+recArea = 1 * w
+
+print("The area of your rectangle is", recArea)
